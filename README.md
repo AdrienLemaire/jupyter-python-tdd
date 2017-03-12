0c067adf0dea1899a3f7fddead14d990583a6c68
Adrien Lemaire <lemaire.adrien@gmail.com>
2017-03-12 09:59:28 +0900

    add initial code to exec python test runner from javascript

--- README.md
+++ README.md
@@ -22,8 +22,27 @@ in a new cell, run:
     Jupyter.utils.load_extensions('tdd')
 
 
-## Install
+## Plan for building the extension
 
-    from notebook.services.config import ConfigManager
-    cm = ConfigManager()
-    cm.update('notebook', {"load_extensions": {"tdd": True}})
+
+### Goal
+* Run unittests in real time to notify user when a test break during a code modification.
+* Show coverage
+* Show visual similar to toc2 extension
+    * panel on the side to display coverage and test results on the fly
+    * top button to hide/show panel. Top button can be colorized to notify user
+
+
+### Thoughts
+
+Do:
+* Need to run a test runner behind. Investigate how to communicate with test runner from extension
+* Test runner should trigger itself automatically with watchdog on the notebook
+* Notebook should be converted on the fly to .py file in order for the test runner to work
+
+
+Don't do:
+
+* Attaching unittests to cells might not be a good idea (test might break due to dependency in other cell)
+* Not a good idea to have a different cell type for unittests. It's still code after all, and we can use the codefolding extension to hide tests
+
--- tdd/main.js
+++ tdd/main.js
@@ -4,6 +4,51 @@ define(function(){
     //---
     load_ipython_extension: function(){
         console.log("I have been loaded ! -- my nb extension");
+
+        // Get all code cells
+        cells = $('.cell.code_cell .inner_cell .CodeMirror-code');
+        current_state = cells.text();
+
+        // Start infinite loop.
+        // Execute test runner cell when state changes
+        //Jupyter.notebook.execute_cells([2])
+        //Instead of adding test runner in a cell and executing it by id, let's exec python from js directly
+        //https://gist.github.com/sanbor/e9c8b57c5759d1ff382a
+		function handle_output(out){
+		    console.log(out);
+		    var res = null;
+		 	// if output is a print statement
+		    if(out.msg_type == "stream"){
+				console.log('It was a stream');
+		 	    res = out.content.data;
+		    }
+		    // if output is a python object
+		    else if(out.msg_type === "execute_result"){
+				console.log('It was a py object');
+		 	    res = out.content.data["text/plain"];
+		    }
+		    // if output is a python error
+		    else if(out.msg_type == "pyerr"){
+				console.log('It was an error');
+		 	    res = out.content.ename + ": " + out.content.evalue;
+		    }
+		    // if output is something we haven't thought of
+		    else{
+		 	    res = "[out type not implemented]";
+		    }
+		    document.getElementById("result_output").value = res;
+		}
+
+        // 1. Find all test class names in current_state
+        // 2. Exec testrunner with class names
+        // 3. Handle output and display somewhere
+        var testclass = 'MyTest';
+        var test_runner = 'a = ' + testclass + '()'\
+                          'suite = unittest.TestLoader().loadTestsFromModule(a)'\
+                          'unittest.TextTestRunner().run(suite)';
+        var kernel = IPython.notebook.kernel;
+        var callbacks = { 'iopub' : {'output' : handle_output}};
+        var msg_id = kernel.execute(code_input, callbacks, {silent:false});
     }
     //---
   };
